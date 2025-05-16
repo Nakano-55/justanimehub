a3259a5fea251cfe888b538d7dcab05d800890e4
Jason Oktario <jasonlibra71@gmail.com>
2025-05-16 13:59:27 +0700

    Update app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   description: "A collaborative anime encyclopedia with multilingual support",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
